405fadd038cbdee3617a8f3dab121d1db01efb00
23flash <77809769+23flash@users.noreply.github.com>
2024-09-19 15:58:02 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.ArrayList;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        readFile("datalog_20230418_143118.csv");
+
+
+
+
+
+    }
+    public static ArrayList<String> readFile(String FileName) throws IOException {
+        ArrayList<String> builder = new ArrayList<>();
+
+
+        File file = new File(FileName);
+        FileReader fr = new FileReader(file);
+        BufferedReader br = new BufferedReader(fr);
+        br.lines().forEach(line -> {builder.add(line );});
+
+        System.out.println(builder.toString());
+        return builder;
+
+    }
+}
+
+
+
